9931d95bdc4175e6eba662295353c115cf75df0d
Kevin <kngo5553@uni.sydney.edu.au>
2018-11-28 17:27:26 +1100

    Responsiveness and Alert
    Responsiveness and Alert added

--- functions.js
+++ functions.js
@@ -4,13 +4,13 @@ function damage(current, extra) {
 	var bot = 1 + (current/100);
 	var top = bot + (extra/100);
 	var increase = (top/bot) - 1;
-    return increase*100;
+    return (increase*100).toFixed(3);
 }
 
 function attack(current,extra){
 	current = parseFloat(current);
 	extra = parseFloat(extra);
-	return 100*extra/current;
+	return (100*extra/current).toFixed(3);
 }
 
 function piercing(current, extra){
@@ -21,7 +21,7 @@ function piercing(current, extra){
 	currDMG = 1 / Math.max(1- current, 0.7);
 	extraDMG = 1 / Math.max(1- extra, 0.7);
 	
-	return 100*(extraDMG - currDMG);
+	return (100*(extraDMG - currDMG)).toFixed(3);
 }
 
 function breakpoint(attack){
@@ -29,7 +29,7 @@ function breakpoint(attack){
 	var blob = (12+attack)/attack;
 	var top = 100*(1.05-blob);
 	var bot = blob - 1;
-	return top/bot;
+	return (top/bot).toFixed(3);
 }
 
 
