da1afd479dec36ce56bb1e2ccd1ce6209547c5cf
Kevin <kngo5553@uni.sydney.edu.au>
2018-11-28 15:12:48 +1100

    First version
    First version

---
+++ functions.js
@@ -0,0 +1,40 @@
+function damage(current, extra) {
+	current = parseFloat(current);
+	extra = parseFloat(extra);
+	var bot = 1 + (current/100);
+	var top = bot + (extra/100);
+	var increase = (top/bot) - 1;
+    return increase*100;
+}
+
+function attack(current,extra){
+	current = parseFloat(current);
+	extra = parseFloat(extra);
+	return 100*extra/current;
+}
+
+function breakpoint(attack){
+	attack = parseFloat(attack);
+	var blob = (12+attack)/attack;
+	var top = 100*(1.05-blob);
+	var bot = blob - 1;
+	return top/bot;
+}
+
+function damageButton() {
+	var current  = document.getElementById("currentdmg").value;
+	var extra  = document.getElementById("extradmg").value;
+	document.getElementById("damageresult").innerHTML = damage(current, extra);
+}
+
+function attackButton() {
+	var current  = document.getElementById("currentatk").value;
+	var extra  = document.getElementById("extraatk").value;
+	document.getElementById("attackresult").innerHTML = attack(current, extra);
+	document.getElementById("breakpointresult").innerHTML = breakpoint(current);
+}
+
+function breakpointButton() {
+	var attack  = document.getElementById("breakpoint").value;
+	document.getElementById("breakpointresult").innerHTML = breakpoint(attack);
+}
